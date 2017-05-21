480e6b488074bd7af156b7cfce6b2b7f25d90d6c
Paree <parisa.rashidirad92@gmail.com>
2017-05-21 18:41:29 +0200

    * set title bar not in search fragment

--- app/src/main/java/com/example/android/forsquare_app/MainActivity.java
+++ app/src/main/java/com/example/android/forsquare_app/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.Window;
@@ -12,64 +13,48 @@ import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    final ActionBar actionBar = getSupportActionBar();
     private TextView mTextMessage;
-
-/*
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_search:
-                    mTextMessage.setText(R.string.title_search);
-                    return true;
-                case R.id.navigation_lists:
-                    mTextMessage.setText(R.string.title_lists);
-                    return true;
-                case R.id.navigation_history:
-                    mTextMessage.setText(R.string.title_history);
-                    return true;
-                case R.id.navigation_me:
-                    mTextMessage.setText(R.string.title_me);
-                    return true;
-            }
-            return false;
-        }
-
-    };
-*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+     //   requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.navigation);
 
+        actionBar.hide();
         bottomNavigationView.setOnNavigationItemSelectedListener
                 (new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         Fragment selectedFragment = null;
+
                         switch (item.getItemId()) {
                             case R.id.navigation_search:
+                                actionBar.hide();
+
                                 selectedFragment = searchFrag.newInstance();
+                               ;
                                 break;
                             case R.id.navigation_history:
                                 selectedFragment = historyFrag.newInstance();
+                                actionBar.show();
+
                                 break;
                             case R.id.navigation_lists:
                                 selectedFragment = listsFrag.newInstance();
+                                actionBar.show();
+
                                 break;
                             case R.id.navigation_me:
                                 selectedFragment = meFrag.newInstance();
+                                actionBar.show();
                                 break;
                         }
                         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
